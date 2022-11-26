bfc8252d51dd31a7d008154a83dfd1c1c556c4d1
RicardPA <ricardo.360.zero@gmail.com>
2022-11-26 14:50:53 -0300

    Classificador

--- Aplicacao.py
+++ Aplicacao.py
@@ -38,6 +38,188 @@ import tqdm
 from PIL import Image, ImageFilter
 from matplotlib.pyplot import figure
 
+
+# --- --- --- --- Declaracao do ShuffleNet --- --- --- ---
+# Variaveis globais necessarias
+root_path_train = './DataBase/train'
+root_path_test = './DataBase/test/'
+# Se o computador tiver cuda usar a GPU
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+#melhor acuracia de teste
+best_acc = 0
+start_epoch = 0
+batch_size = 128
+weight_decay = 5e-4
+momentum = 0.9
+learning_rate = 0.01
+epoch_size = 60
+
+"""
+Nome: ShuffleNet
+Funcao: Implemtar aprendizado profundo na aplicacao, utilizando o
+shufflenet como IA.
+Descricao: Construcao do algoritmo shufflenet.
+"""
+class ShuffleBlock(nn.Module):
+    def __init__(self, groups):
+        super(ShuffleBlock, self).__init__()
+        self.groups = groups
+    def forward(self, x):
+        N,C,H,W = x.size()
+        g = self.groups
+        return x.view(N, g, C//g, H, W).permute(0, 2, 1, 3, 4).reshape(N, C, H, W)
+
+class Bottleneck(nn.Module):
+    def __init__(self, in_planes, out_planes, stride, groups):
+        super(Bottleneck, self).__init__()
+        self.stride = stride
+        mid_planes = int(out_planes/4)
+        g = 1 if in_planes == 24 else groups
+        self.conv1 = nn.Conv2d(in_planes, mid_planes, kernel_size=1, groups=g, bias=False)
+        self.bn1 = nn.BatchNorm2d(mid_planes)
+        self.shuffle1 = ShuffleBlock(groups= g)
+        self.conv2 = nn.Conv2d(mid_planes, mid_planes, kernel_size=3, stride=stride, padding=1, groups=mid_planes, bias=False)
+        self.bn2 = nn.BatchNorm2d(mid_planes)
+        self.conv3 = nn.Conv2d(mid_planes, out_planes, kernel_size=1, groups=groups, bias=False)
+        self.bn3 = nn.BatchNorm2d(out_planes)
+        self.shortcut = nn.Sequential()
+        if stride==2:
+            self.shortcut = nn.Sequential(nn.AvgPool2d(3,stride=2, padding =1))
+
+    def forward(self,x):
+        out = functions.relu(self.bn1(self.conv1(x)))
+        out = self.shuffle1(out)
+        out = functions.relu(self.bn2(self.conv2(out)))
+        out = self.bn3(self.conv3(out))
+        res = self.shortcut(x)
+        out = functions.relu(torch.cat([out,res], 1)) if self.stride==2 else functions.relu(out+res)
+        return out
+
+class ShuffleNet(nn.Module):
+    def __init__(self, cfg):
+        super(ShuffleNet, self).__init__()
+        out_planes = cfg['out_planes']
+        num_blocks = cfg['num_blocks']
+        groups = cfg['groups']
+        self.conv1 = nn.Conv2d(3, 24, kernel_size=1, bias = False)
+        self.bn1 = nn.BatchNorm2d(24)
+        self.in_planes = 24
+        self.layer1 = self._make_layer(out_planes[0], num_blocks[0], groups)
+        self.layer2 = self._make_layer(out_planes[1], num_blocks[1], groups)
+        self.layer3 = self._make_layer(out_planes[2], num_blocks[2], groups)
+        self.linear = nn.Linear(out_planes[2], 5) #10 as there are 10 classes
+
+    def _make_layer(self, out_planes, num_blocks, groups):
+        layers = []
+        for i in range(num_blocks):
+            stride = 2 if i == 0 else 1
+            cat_planes = self.in_planes if i==0 else 0
+            layers.append(Bottleneck(self.in_planes, out_planes-cat_planes, stride=stride, groups=groups))
+            self.in_planes = out_planes
+        return nn.Sequential(*layers)
+
+    def forward(self,x):
+        out = functions.relu(self.bn1(self.conv1(x)))
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = functions.avg_pool2d(out, 4)
+        out = out.view(out.size(0), -1)
+        out = self.linear(out)
+        return out
+
+def ShuffleNetG2():
+    cfg = {'out_planes': [200, 400, 800],
+         'num_blocks': [4, 8, 4],
+         'groups': 2
+         }
+    return ShuffleNet(cfg)
+
+def ShuffleNetG3():
+    cfg = {'out_planes': [240, 480, 960],
+         'num_blocks': [4, 8, 4],
+         'groups': 3
+         }
+    return ShuffleNet(cfg)
+
+#Shufflenet com grupos = 2
+net2 = ShuffleNetG2()
+
+#Shufflenet com grupos = 3
+net3 = ShuffleNetG3()
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net3.parameters(), lr = learning_rate ,momentum= momentum, weight_decay=weight_decay)
+
+def get_loss_acc(dataloader, is_test_dataset = True):
+    net3.eval()
+    # dataloader = test_loader if is_test_dataset else train-loader
+    global batch_size
+    n_correct = 0
+    n_total = 0
+    test_loss = 0
+    with torch.no_grad():
+        for batch_size, (inputs, targets) in enumerate(dataloader):
+            inputs, targets = inputs.to(device), targets.to(device)
+            outputs = net3(inputs)
+            test_loss += criterion(outputs, targets).item()
+            _, predicted = outputs.max(1)
+            n_correct +=predicted.eq(targets).sum().item()
+            n_total += targets.shape[0]
+    return test_loss/(batch_size+1), n_correct/n_total
+
+def train_model(dataloader):
+    net3.train()
+    global batch_size
+    train_loss = 0
+    n_correct = 0
+    n_total = 0
+
+    for batch_size, (inputs, targets) in enumerate(dataloader):
+        inputs, targets = inputs.to(device), targets.to(device)
+        optimizer.zero_grad()
+        outputs = net3(inputs)
+        loss = criterion(outputs, targets)
+        loss.backward()
+        optimizer.step()
+        train_loss += loss.item()
+        _, predicted = outputs.max(1)
+        n_correct +=predicted.eq(targets).sum().item()
+        n_total += targets.shape[0]
+    return train_loss/(batch_size+1), n_correct/n_total
+
+def save_best_model(epoch, dataloader):
+    global best_acc
+    net3.eval()
+    test_loss = 0
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for batch_idx, (inputs, targets) in enumerate(dataloader):
+            inputs, targets = inputs.to(device), targets.to(device)
+            outputs = net3(inputs)
+            loss = criterion(outputs, targets)
+            test_loss += loss.item()
+            _, predicted = outputs.max(1)
+            total += targets.size(0)
+            correct +=predicted.eq(targets).sum().item()
+            print(batch_idx, len(dataloader), 'Loss: %.3f | Acc: %0.3f%% (Correct classifications %d/Total classifications %d)' % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
+
+            #save checkpoint
+            acc =100.*correct/total
+            if acc>best_acc:
+                print('Saving...')
+                state = {'net': net3.state_dict(),
+                         'acc': acc,
+                         'epoch': epoch}
+            if not os.path.isdir('checkpoint'):
+                os.mkdir('checkpoint')
+            torch.save(state, './checkpoint/ckpt.pth')
+            torch.save(net3, './checkpoint/net3.pth')
+            best_acc = acc
+
+# --- --- --- --- Aplicacao --- --- --- ---
+
 warnings.filterwarnings("ignore")
 
 clicked = False # Verificar comeco de corte
@@ -88,7 +270,10 @@ def comparar():
     fechar_menu()
 
     # Obter primeiro arquivo selecionado pelo usuario
-    fileNameFirst = filedialog.askopenfilename(title = "Selecione a primeira imagem para comparação", filetypes= extensionFiletypes)
+    fileNameFirst = filedialog.askopenfilename(
+                    title = "Selecione a primeira imagem para comparação",
+                    filetypes= extensionFiletypes)
+
     firstImg = cv2.imread(fileNameFirst)
 
     # Rcomecar o menu caso nao exista imagem
@@ -112,7 +297,10 @@ def comparar():
                 extensionFiletypes = [('JPG', "*.jpg")]
 
         # Obter segundo arquivo selecionado pelo usuario
-        fileNameSecond = filedialog.askopenfilename(title = "Selecione a segunda imagem para comparação", filetypes= extensionFiletypes)
+        fileNameSecond = filedialog.askopenfilename(
+                        title = "Selecione a segunda imagem para comparação",
+                        filetypes= extensionFiletypes)
+
         secondImg = cv2.imread(fileNameSecond)
 
         # Rcomecar o menu caso nao exista imagem
@@ -125,17 +313,20 @@ def comparar():
             secondImg = cv2.equalizeHist(secondImg)
 
             # Comparar imagens
-            resultComparation = cv2.matchTemplate(secondImg, firstImg, cv2.TM_CCORR_NORMED)
-            (valueMin, valueMax, comparationMin, comparationMax) = cv2.minMaxLoc(resultComparation)
+            resultComparation = cv2.matchTemplate(secondImg, firstImg,
+                                                  cv2.TM_CCORR_NORMED)
+
+            (vMin, vMax, cMin, cMax) = cv2.minMaxLoc(resultComparation)
 
             # Obter coordenadas do local de semelhanca da imagem
-            (start_x, start_y) = comparationMax
+            (start_x, start_y) = cMax
             end_x = start_x + firstImg.shape[1]
             end_y = start_y + firstImg.shape[0]
 
             # Apresentar imagem com comparacao
             resultImage = cv2.imread(fileNameSecond)
-            cv2.rectangle(resultImage, (start_x, start_y), (end_x, end_y), (255, 0, 0), 2)
+            cv2.rectangle(resultImage, (start_x, start_y),
+                          (end_x, end_y), (255, 0, 0), 2)
             cv2.imshow("Resultado da comparação", resultImage)
             saveImage = resultImage.copy()
 
@@ -214,7 +405,9 @@ def salvar_imagem_gerada():
         extensionFiletypes = [('JPG', "*.jpg")]
 
     # Obter nome do arquivo e posicao do arquivo
-    filepathAndFile = filedialog.asksaveasfilename(title = "Selecione a pasta para armazenar o arquivo", filetypes = extensionFiletypes)
+    filepathAndFile = filedialog.asksaveasfilename(
+                    title = "Selecione a pasta para armazenar o arquivo",
+                    filetypes = extensionFiletypes)
 
     for char in filepathAndFile[::-1]:
         if(char != "/"):
@@ -293,35 +486,51 @@ Nome: shufflenet
 Funcao: Aplicar o aprendizado shufflenet
 """
 def shufflenet():
-    #cv2.resize(secondImg, (224, 224), interpolation = cv2.INTER_AREA)
+    transform_train = transforms.Compose(transforms=[transforms.Pad(4),
+                                         transforms.RandomHorizontalFlip(),
+                                         transforms.ToTensor(),
+                                         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])
 
-    device = 'cuda' if torch.cuda.is_available() else 'cpu' #to set the device to be used by pytorch
-    best_acc = 0 #best test accuracy
-    start_epoch = 0 #start from epoch 0 or last checkpoint epoch
-    batch_size = 128
-    weight_decay = 5e-4
-    momentum = 0.9
-    learning_rate = 0.01
-    epoch_size = 60
+    transform_test = transforms.Compose(transforms=[transforms.ToTensor(),
+                                        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])
 
-    LABEL_MAP = {0:'0', 1:'1', 2:'2', 3:'3'}
+    train_set = dsets.ImageFolder(root=root_path_train, transform=transform_train)
+    test_set = dsets.ImageFolder(root=root_path_test, transform=transform_test)
 
-    figure(num=None, figsize=(5, 5), dpi=150, facecolor='w', edgecolor='k')
+    train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=0, drop_last=False)
+    test_loader = torch.utils.data.DataLoader(test_set, batch_size=batch_size, shuffle=False, num_workers=0, drop_last=False)
 
-    data_train = [[],[],[],[],[]]
+    f = open("./dadosShufflenet.txt", "w")
 
-    for i in range(0, 5):
-        name_files = glob.glob("./DataBase/train/" + str(i) + "/*.png")
-        for name in name_files:
-            data_train[i].append(cv2.imread(name))
+    EPOCH = epoch_size
+    training_time = dt.datetime.now()
+    start = dt.datetime.now()
+    start_epoch = 0
+    train_accuracy_list = []
+    test_accuracy_list = []
 
-    transform_train = transforms.Compose(transforms=[transforms.Pad(4), transforms.RandomHorizontalFlip(), transforms.ToTensor(), transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])
-    transform_test = transforms.Compose(transforms=[transforms.ToTensor(), transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])
+    print('INICIO DO TREINAMENTO')
+    for epoch_i in range(start_epoch, start_epoch + EPOCH):
+      global accuracy_list
+      current_learning_rate = [i['lr'] for i in optimizer.param_groups][0]
+      # Mostrar e arquivar dados de aprendizado
+      f.write('Batch Size' + str(batch_size) + '(' + str((dt.datetime.now() - start).seconds) + ')\n\nEpoch: ' + str(epoch_i +1) + '/' + str(EPOCH+start_epoch) + ' | Current Learning Rate: ' + str(current_learning_rate))
+      print('Batch Size', batch_size, '(%0.2fs)\n\nEpoch: %d/%d | Current Learning Rate: %.4f ' % ((dt.datetime.now() - start).seconds, epoch_i +1, EPOCH+start_epoch , current_learning_rate))
 
-    # Passar a imagem dentro do transform_trai() e transform_test() 
-    train_set = transform_train()
+      start = dt.datetime.now()
+      test_loss, test_acc = get_loss_acc(train_loader)
+      train_loss, train_acc = train_model(train_loader)
+      train_accuracy_list.append(train_acc*100)
+      test_accuracy_list.append(test_acc*100)
+      save_best_model(epoch_i, train_loader)
 
-    print("Em construcao")
+      # Mostrar e arquivar dados de aprendizado
+      print('Train Loss: %.3f | Acc: %.3f%% \nTest Loss: %0.3f | Acc: %0.3f%% \n\n' % (train_loss, train_acc*100, test_loss, test_acc*100))
+      f.write('Train Loss: ' + str(train_loss) + ' | Acc: ' + str(train_acc*100) + ' \nTest Loss: ' + str(test_loss) + ' | Acc: ' + str(test_acc*100) + ' \n\n')
+
+    print('\n\nTotal Training time: %0.2f minutes ' %((dt.datetime.now() - training_time).seconds/60))
+    f.write('\n\nTotal Training time: ' + str((dt.datetime.now() - training_time).seconds/60) + ' minutes ')
+    f.close()
 
 
 
@@ -364,19 +573,23 @@ def construir_menu_principal():
     screen.pack()
 
     # Criar botao responsavel por chamar o corte de uma imagem
-    btn_abrir_arquivo = Tk.Button(root, text = "Cortar Imagem", padx = 1, pady = 1, fg = "white", bg = "#00006F", command = abrir_imagem)
+    btn_abrir_arquivo = Tk.Button(root, text = "Cortar Imagem", padx = 1,
+                 pady = 1, fg = "white", bg = "#00006F", command = abrir_imagem)
     btn_abrir_arquivo.place(relwidth = 0.22, relheight = 0.8, relx = 0.02, rely = 0.1)
 
     # Criar botao resonsavel por selecionar imagens que serao comparadas
-    btn_comparar = Tk.Button(root, text = "Comparar", padx = 1, pady = 1, fg = "white", bg = "#00006F", command = comparar)
+    btn_comparar = Tk.Button(root, text = "Comparar", padx = 1, pady = 1,
+                               fg = "white", bg = "#00006F", command = comparar)
     btn_comparar.place(relwidth = 0.20, relheight = 0.8, relx = 0.25, rely = 0.1)
 
     # Criar botao resonsavel por aumentar a quantidade de dados
-    btn_aumentar_dados = Tk.Button(root, text = "Aumentar dados", padx = 1, pady = 1, fg = "white", bg = "#00006F", command = aumentar_dados)
+    btn_aumentar_dados = Tk.Button(root, text = "Aumentar dados", padx = 1,
+                                   pady = 1, fg = "white", bg = "#00006F", command = aumentar_dados)
     btn_aumentar_dados.place(relwidth = 0.27, relheight = 0.8, relx = 0.46, rely = 0.1)
 
     # Criar botao resonsavel por aumentar a quantidade de dados
-    btn_classificadores = Tk.Button(root, text = "Classificadores", padx = 1, pady = 1, fg = "white", bg = "#00006F", command = construir_menu_classificador)
+    btn_classificadores = Tk.Button(root, text = "Classificadores", padx = 1,
+                                    pady = 1, fg = "white", bg = "#00006F", command = construir_menu_classificador)
     btn_classificadores.place(relwidth = 0.24, relheight = 0.8, relx = 0.74, rely = 0.1)
 
 """
@@ -395,11 +608,13 @@ def construir_menu_salvar_imagem():
     screen.pack()
 
     # Criar botao com opcao para salvar
-    btn_salvar = Tk.Button(root, text = "Salvar Imagem", padx = 1, pady = 1, fg = "white", bg = "GREEN", command = salvar_imagem_gerada)
+    btn_salvar = Tk.Button(root, text = "Salvar Imagem", padx = 1, pady = 1,
+                           fg = "white", bg = "GREEN", command = salvar_imagem_gerada)
     btn_salvar.place(relwidth = 0.4, relheight = 0.8, relx = 0.02, rely = 0.1)
 
     # Criar botao com opcao de nao salvar
-    btn_nao_salvar = Tk.Button(root, text = "Não Salvar", padx = 1, pady = 1, fg = "white", bg = "RED", command = nao_salvar_imagem_gerada)
+    btn_nao_salvar = Tk.Button(root, text = "Não Salvar", padx = 1, pady = 1,
+                               fg = "white", bg = "RED", command = nao_salvar_imagem_gerada)
     btn_nao_salvar.place(relwidth = 0.4, relheight = 0.8, relx = 0.585, rely = 0.1)
 
 """
@@ -417,7 +632,8 @@ def construir_menu_classificador():
     screen.pack()
 
     # Criar botao com opcao para salvar
-    btn_shufflenet = Tk.Button(root, text = "Shufflenet", padx = 1, pady = 1, fg = "white", bg = "#00006F", command = shufflenet)
+    btn_shufflenet = Tk.Button(root, text = "Shufflenet", padx = 1, pady = 1,
+                               fg = "white", bg = "#00006F", command = shufflenet)
     btn_shufflenet.place(relwidth = 0.4, relheight = 0.8, relx = 0.02, rely = 0.1)
 
 construir_menu_principal() # Chamar primeira criacao/configuracao de tela
